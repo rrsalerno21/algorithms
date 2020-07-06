dae67b873adf02830fc887476dc012178be1caf7
Rocky <rrsalerno21@gmail.com>
2020-07-06 15:59:30 -0700

    added algos folder and log-even-numbers

---
+++ algos/log-even-nums.js
@@ -0,0 +1,11 @@
+function logEvenNums(max) {
+    for (var i = 0; i <= max; i++) {
+        if (i % 2 === 0) {
+            console.log(i)
+        }
+    }
+}
+
+// logEvenNums(6);
+// logEvenNums(3);
+// logEvenNums(15);
