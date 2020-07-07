6278974610cd0be9ed476b272ded19fd88dab871
Rocky <rrsalerno21@gmail.com>
2020-07-07 09:53:19 -0700

    added reverse-words

---
+++ algos/reverse-words.js
@@ -0,0 +1,6 @@
+// Write code to create a function that takes a string and returns a new string with the order of the words reversed.
+function reverseWords(str) {
+    return str.split(' ').reverse().join(' ');
+}
+
+// console.log(reverseWords('Test me out, dude'));
