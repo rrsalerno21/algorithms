f69e00ef30c9de84ebb52d67373e1d52859b580f
Rocky <rrsalerno21@gmail.com>
2020-07-07 09:09:22 -0700

    added factorial

---
+++ algos/factorial.js
@@ -0,0 +1,13 @@
+function factorial(num) {
+    
+    let fact = 1;
+    for (var i = num; i > 1; i--) {
+        fact *= i;
+    }
+    
+    console.log(fact);
+}
+
+factorial(0);
+factorial(3);
+factorial(15);
